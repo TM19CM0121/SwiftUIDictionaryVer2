11cb0a8a1cf33cd73959da30391979fa2015d110
TM19CM0121 <lbxmasa@icloud.com>
2020-02-18 23:37:25 +0900

    Initial Commit

---
+++ SwiftUIDictionaryVer2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDictionaryVer2
+//
+//  Created by T.R.Marigold on 2020/02/18.
+//  Copyright © 2020 高良昌辰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
